b486cf04659ef66622d67945c5d76c775d5fc2c1
unknown <aynazsln2005@gmail.com>
2025-04-09 19:59:41 +0330

    Add copy to the methods

--- src/db/Database.java
+++ src/db/Database.java
@@ -11,13 +11,13 @@ public class Database {
 
     public static void add(Entity e) {
         e.id = numberOfId++;
-        entities.add(e);
+        entities.add(e.copy());
     }
 
     public static Entity get(int id) {
         for (Entity e : entities) {
             if (e.id == id) {
-                return e;
+                return e.copy();
             }
         }
         throw new EntityNotFoundException(id);
@@ -36,7 +36,7 @@ public class Database {
     public static void update(Entity newEntity) {
         for (int i = 0; i < entities.size(); i++) {
             if (entities.get(i).id == newEntity.id) {
-                entities.set(i, newEntity);
+                entities.set(i, newEntity.copy());
                 return;
             }
         }
